61d30d85c4655118403529f8d4ebf8f99e7baab0
Akima-zed <julie.regereau@gmail.com>
2025-12-16 11:14:47 +0100

    feat(user): ajout du service et du contrôleur utilisateur (UserService, UserController)

---
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.openclassrooms.mddapi.controller;
+
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> user = userService.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
